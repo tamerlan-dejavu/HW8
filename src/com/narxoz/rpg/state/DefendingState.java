1564100ff939a9a2e5f6a9683fc52e871e80f12f
tamerlan-dejavu <tamerlan.dejavu@gmail.com>
2026-04-20 09:36:53 +0500

    I implemented defending state

--- src/com/narxoz/rpg/state/DefendingState.java
+++ src/com/narxoz/rpg/state/DefendingState.java
@@ -1,5 +1,41 @@
 package com.narxoz.rpg.state;
 
-public class DefendingState {
-    
+import com.narxoz.rpg.combatant.Hero;
+
+public class DefendingState implements HeroState {
+    private int turnsRemaining = 2; 
+
+    @Override
+    public String getName() {
+        return "Defending";
+    }
+
+    @Override
+    public int modifyOutgoingDamage(int basePower) {
+        return basePower / 2;
+    }
+
+    @Override
+    public int modifyIncomingDamage(int rawDamage) {
+        return rawDamage / 2;
+    }
+
+    @Override
+    public void onTurnStart(Hero hero) {
+        System.out.println(">>> " + hero.getName() + " raises their shield! (Duration: " + turnsRemaining + ")");
+    }
+
+    @Override
+    public void onTurnEnd(Hero hero) {
+        turnsRemaining--;
+        if (turnsRemaining <= 0) {
+            System.out.println(">>> " + hero.getName() + " lowers their shield.");
+            hero.setState(new NormalState());
+        }
+    }
+
+    @Override
+    public boolean canAct() {
+        return true;
+    }
 }
